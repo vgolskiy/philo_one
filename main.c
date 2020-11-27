9141311c3d29b53d833032a4b2014a21378da576
vgolskiy <v.golskiy@yandex.ru>
2020-11-27 13:42:39 +0300

    organized better

--- main.c
+++ main.c
@@ -1,23 +1,5 @@
 #include "phil.h"
 
-static int	pthread_result(t_st *st)
-{
-	int		status;
-	int		i;
-
-	i = -1;
-	while (++i < st->qty)
-	{
-		pthread_join(st->ph[i].ph_id, (void *)&status);
-		if (status)
-			return (free_all(st) && EXIT_FAILURE);
-		pthread_join(st->ph[i].checker_id, (void *)&status);
-		if (status)
-			return (free_all(st) && EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
-
 int			main(int argc, char **argv)
 {
 	t_st	st;
